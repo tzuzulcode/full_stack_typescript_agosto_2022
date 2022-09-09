6f56793fcfe9a8eefc9d8a0b8f7d776aae5e7dee
Tzuzul <mail@tzuzulcode.com>
2022-09-08 22:16:43 -0500

    Modificadores de acceso

--- src/06 classes/index.ts
+++ src/06 classes/index.ts
@@ -4,7 +4,26 @@ type Color = 'black' | 'white'
 type File = 'A'|'B'|'C'|'D'|'E'|'F'|'G'|'H'
 type Rank = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
 
-class Game{}
+class Game{
+    private pieces = Game.makePieces()
+
+    // Static: Significa que no es necesario instanciar la clase para utilizar el método
+    // Cuando se combina private y static significa que será un método que ejecutaremo al momento de instanciar la clase
+    // Método de un solo uso
+    private static makePieces(){
+        return [
+            new King('white','E',1),
+            new King('black','E',8),
+            new Queen('white','D',1),
+            new Queen('black','D',8),
+            // Reto: Agregar las piezas al tablero
+            
+        ]
+    }
+}
+
+const game = new Game()
+
 
 abstract class Piece{
     protected position: Position
@@ -15,6 +34,14 @@ abstract class Piece{
     ){
         this.position = new Position(file,rank)
     }
+
+    // Implementación por defecto. Pero se puede sobreescribir
+    moveTo(position:Position){
+        this.position = position
+    }
+
+    // abstract: Le dice a las subclases que tienen que implementar este método. Siguiendo la firma dada.
+    abstract canMoveTo(position:Position):boolean
 }
 
 class Position{
@@ -22,9 +49,46 @@ class Position{
         private file:File,
         private rank:Rank
     ){}
+
+    distanceFrom(position:Position){
+        return {
+            rank: Math.abs(position.rank-this.rank),
+            file: Math.abs(position.file.charCodeAt(0)-this.file.charCodeAt(0)),
+        }
+    }
 }
 
-class King extends Piece {}
-class Queen extends Piece {}
-class Bishop extends Piece {}
-class Pawn extends Piece {}
+class King extends Piece {
+    // Implementación del método abstracto
+    canMoveTo(position: Position): boolean {
+        let distance = this.position.distanceFrom(position)
+        return distance.rank<2 && distance.file<2
+    }
+}
+
+// Reto: Implementar la clase abstracta Piece
+class Queen extends Piece {
+    canMoveTo(position: Position): boolean {
+        throw new Error("Method not implemented.")
+    }
+}
+class Bishop extends Piece {
+    canMoveTo(position: Position): boolean {
+        throw new Error("Method not implemented.")
+    }
+}
+class Knight extends Piece {
+    canMoveTo(position: Position): boolean {
+        throw new Error("Method not implemented.")
+    }
+}
+class Rook extends Piece {
+    canMoveTo(position: Position): boolean {
+        throw new Error("Method not implemented.")
+    }
+}
+class Pawn extends Piece {
+    canMoveTo(position: Position): boolean {
+        throw new Error("Method not implemented.")
+    }
+}
